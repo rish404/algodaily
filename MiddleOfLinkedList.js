154ff7fac633e6d46f4c14b30443dee11d1da3e2
Rish Agarwal <rish.destiny@gmail.com>
2021-06-28 16:48:45 +0530

    MiddleOfLinkedList

---
+++ MiddleOfLinkedList.js
@@ -0,0 +1,30 @@
+/*
+initial state
+f
+1 -> 2 -> 3 -> 4 -> 5
+s
+
+1st loop
+		  f
+1 -> 2 -> 3 -> 4 -> 5
+     s
+	 
+2nd loop
+		            f
+1 -> 2 -> 3 -> 4 -> 5
+          s
+
+when f reach end of the linked list, s will be at the middle.
+
+f = fast pointer
+s = slow pointer
+*/
+
+var middleNode = function(head) {
+    let fast = slow = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
