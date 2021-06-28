f4b4000089c61b0ac098b31bf771f59ee89c7666
Rish Agarwal <rish.destiny@gmail.com>
2021-06-28 17:07:56 +0530

    MergeTwoSortedLinkedLists - 2

--- MergeTwoSortedLinkedLists.js
+++ MergeTwoSortedLinkedLists.js
@@ -12,3 +12,18 @@ var mergeTwoLists = function(l1, l2) {
     
     return l1 || l2
 };
+
+
+
+//Code 2
+const mergeTwoLists = (l1, l2) => {
+    if(!l1 || !l2) return l1 || l2
+    if(l1.val < l2.val){
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    }
+    else {
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
+    }
+}
